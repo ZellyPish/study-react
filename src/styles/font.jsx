ce353b8b4d0e667acaec6f65fbdd5390c264eba2
Jongchan Kim <ehxhfl21@gmail.com>
2022-08-24 23:41:30 +0900

    set font

---
+++ src/styles/font.jsx
@@ -0,0 +1,8 @@
+import { css } from '@emotion/react'
+
+const font = css`
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap');
+  * {
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+`
