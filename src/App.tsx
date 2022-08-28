2e6fb412d0e72d89a01e85584b89cff026dcd116
Jongchan Kim <ehxhfl21@gmail.com>
2022-08-28 16:57:29 +0900

    set font and reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Global } from '@emotion/react'
 import { FC } from 'react'
+import font from 'styles/font'
 import reset from 'styles/reset'
 
 const App: FC = () => {
   return (
     <>
       <Global styles={reset} />
+      <Global styles={font} />
       <div>
-        App
+        안녕하세요
       </div>
     </>
   )
---
+++ src/styles/font.jsx
@@ -0,0 +1,10 @@
+import { css } from '@emotion/react'
+
+const font = css`
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap');
+  html {
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+`
+
+export default font
